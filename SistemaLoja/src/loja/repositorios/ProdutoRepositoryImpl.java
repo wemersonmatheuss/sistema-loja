2a8b2cf2aec14719befc26539856f5c055939f7f
Wemerson Matheus <wemersonmthdev@gmail.com>
2025-03-30 18:36:55 -0300

    Update ProdutoRepositoryImpl.java

--- SistemaLoja/src/loja/repositorios/ProdutoRepositoryImpl.java
+++ SistemaLoja/src/loja/repositorios/ProdutoRepositoryImpl.java
@@ -8,12 +8,12 @@ import java.util.List;
 public class ProdutoRepositoryImpl implements ProdutoRepository {
 	 private List<Produto> produtos = new ArrayList<>();
 
-	    @Override
+	
 	    public void adicionarProduto(Produto produto) {
 	        produtos.add(produto);
 	    }
 
-	    @Override
+	    
 	    public Produto buscarProduto(int id) {
 	        return produtos.stream().filter(p -> p.getId() == id).findFirst().orElse(null);
 	    }
